79955902e971e2ca2eca26335ab6c1f4762da17a
Tomas Gentile <ltomass@hotmail.com>
2022-04-15 19:20:59 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '08cacae5d6cf87b6968ab0ce7c672293'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4c9db67f-f188-413b-9b09-9ee796880fa9'
+};
